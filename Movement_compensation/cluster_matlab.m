c8eb7d9e248e86d9db70ffa7b5f11976a74aa810
Kristoffer Hansen <s134082@student.dtu.dk>
2016-07-18 08:59:12 +0200

    Change of file structure in movement_compensation

--- Movement_compensation/cluster_matlab.m
+++ Movement_compensation/cluster_matlab.m
@@ -70,3 +70,38 @@ for i = 1:tasks
     mov_y(i,:) = data{1,i}{1,1}{1};
     mov_x(i,:) = data{1,i}{1,1}{2};
 end
+
+
+%% Imaging
+
+figure(); plot((mov_y_mean()')); legend('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15');
+xlabel('Time (s)'); ylabel('Axial displacement (\mu m)'); 
+title('Axial displacement');
+ylim([-8,12]);% xlim([0,40]);
+set(gca,'Xtick',linspace(0,50,6)); set(gca,'XtickLabel',linspace(0,1,6));
+set(gca,'Ytick',linspace(-8,12,6)); set(gca, 'YTickLabel',linspace(0,250,6));
+%%
+figure(); plot((mov_x_mean()')); legend('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15');
+xlabel('Time (s)'); ylabel('Lateral displacement (\mu m)'); 
+title('Lateral displacement');
+ylim([-3,3]);% xlim([0,40]);
+set(gca,'Xtick',linspace(0,50,6)); set(gca,'XtickLabel',linspace(0,1,6));
+set(gca,'Ytick',linspace(-3,3,6)); set(gca, 'YTickLabel',linspace(0,250,6));
+
+%%
+% Full image
+img_disp = load_img_B_mode(1);
+figure(1); clf;
+norm = max(abs(img_disp(:)));
+limg=20*log10(abs(img_disp)/norm);
+imagesc(limg,[-40 0]);% xlim([1 size(img,2)]); ylim([1 size(img,1)]);
+colormap('gray'); xlabel('Lateral (mm)'); ylabel('Axial (mm)'); %title('B-mode image');
+set(gca,'Ytick',linspace(1,1960,6)); set(gca,'YtickLabel',linspace(0,25,6));
+set(gca,'Xtick',linspace(1,280,6)); set(gca, 'XTickLabel',linspace(0,12,6));
+hold on
+scatter_img = scatter(X(:),Y(:))
+set(scatter_img,'SizeData', 50); % size of dots
+set(scatter_img,'MarkerFacecolor','flat'); % appearance of dots
+for i = 1:15
+    text(X(i)-3,Y(i)-35, int2str(i),'Color','r','FontSize',15,'FontWeight','bold');
+end
